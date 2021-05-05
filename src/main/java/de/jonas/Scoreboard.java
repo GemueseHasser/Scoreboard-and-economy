e43f46fa5aecfdf3519e953ef9b1aba340f870dd
Jonas <jonas.lobe.2006@outlook.com>
2021-05-05 13:13:20 +0200

    Initial commit

---
+++ src/main/java/de/jonas/Scoreboard.java
@@ -0,0 +1,17 @@
+package de.jonas;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Scoreboard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
